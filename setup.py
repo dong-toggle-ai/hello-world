e38326701a9891cf6b3056a6803dc58957b2ea68
Dong Wang <dong@toggle.ai>
2023-01-27 10:09:17 +0000

    Add existing project files to Git

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='hello-world',
+    packages=find_packages(),
+    version='0.1.0',
+    description='a hello world project',
+    author='dong@toggle.ai',
+)
